f0ee13894ce8185c6953066224dffdbb8eebc150
Violet Yeh <violetsyeh@gmail.com>
2018-05-29 17:35:52 -0700

    refactored some code/adding session

--- server.py
+++ server.py
@@ -15,93 +15,100 @@ app = Flask(__name__)
 # Required to use Flask sessions and the debug toolbar
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 
-
 # Normally, if you use an undefined variable in Jinja2, it fails
 # silently. This is horrible. Fix this so that, instead, it raises an
 # error.
 app.jinja_env.undefined = StrictUndefined
-sp_oauth = oauth2.SpotifyOAuth(Spotify_Client_Id, Spotify_Client_Secret,
-                            Redirect_Uri,scope=SCOPE, cache_path=CACHE)
+
+sp_oauth = oauth2.SpotifyOAuth(client_id=Spotify_Client_Id, client_secret=Spotify_Client_Secret,
+                                redirect_uri=Redirect_Uri,scope=SCOPE, cache_path=CACHE)
+auth_url = sp_oauth.get_authorize_url()
 
 @app.route('/')
 def index():
     """Homepage."""
+
     access_token = ""
-    sp_oauth = oauth2.SpotifyOAuth(client_id=Spotify_Client_Id, client_secret=Spotify_Client_Secret,
-                                redirect_uri=Redirect_Uri,scope=SCOPE, cache_path=CACHE)
 
     token_info = sp_oauth.get_cached_token()
-    auth_url = sp_oauth.get_authorize_url()
 
     if token_info:
-        print "cached_token found"
-        print '========='
+        # print "cached_token found"
+        # print '========='
         access_token = token_info['access_token']
         sp = spotipy.Spotify(auth=access_token)
         user = sp.current_user()
-        return render_template('homepage.html', user=user)
+        return render_template('homepage.html', user=user, auth_url=auth_url)
+
+    elif sp_oauth.is_token_expired(token_info):
+        refresh()
+
     else:
         url = request.url
         code = sp_oauth.parse_response_code(url)
-        print code
-        print '====================code'
+        # print code
+        # print '====================code'
 
         if code:
-            print "found spotify auth code in request url"
+            # print "found spotify auth code in request url"
             token_info = sp_oauth.get_access_token(code)
             access_token = token_info['access_token']
 
     if access_token:
-        print "access token exists"
-        sp = spotipy.Spotify(access_token)
-        results = sp.current_user()
-        return results
-    else:
-        auth_url = sp_oauth.get_authorize_url()
-        print auth_url
-        print '========'
-        return render_template("homepage.html", auth_url=auth_url)
+        return redirect('/login')
+    #     # print "access token exists"
+    #     sp = spotipy.Spotify(access_token)
+    #     # results = sp.current_user()
+    #     results = sp.featured_playlists(locale=None, country=None, timestamp=None, limit=5, offset=0)
+    #     if results:
+    #         playlists = []
+    #         i = 0
+    #
+    #         for i in range(limit):
+    #             playlists.append(results['playlists']['items'][i]['uri'])
+    #     return render_template('show-featured-playlists.html', auth_url=auth_url, playlists=playlists)
+    # else:
+    #     return render_template("homepage.html", auth_url=auth_url)
 
-# def getauthURI():
-#     auth_url = sp_oauth.get_authorize_url()
-#     return auth_url
-    # token_info = sp_oauth.get_cached_token()
-    # if not token_info:
-    #     auth_url = sp_oauth.get_authorize_url()
-    #     print(auth_url)
-    #     response = input('Paste the above link into your browser, then paste the redirect url here: ')
-    #
-    # code = sp_oauth.parse_response_code(response)
-    # token_info = sp_oauth.get_access_token(code)
-    #
-    # token = token_info['access_token']
-    #
-    # sp = spotipy.Spotify(auth=token)
 
 @app.route('/login')
 def login():
     access_token = ""
-    sp_oauth = oauth2.SpotifyOAuth(client_id=app.Spotify_Client_Id, client_secret=app.Spotify_Client_Secret,
-                                redirect_uri=Redirect_Uri,scope=SCOPE, cache_path=CACHE)
 
     token_info = sp_oauth.get_cached_token()
-    auth_url = sp_oauth.get_authorize_url()
 
     url = request.url
     code = sp_oauth.parse_response_code(url)
-    print code
-    print '===================='
+    # print code
+    # print '===================='
 
     if code:
-        print "found spotify auth code in request url"
+        # print "found spotify auth code in request url"
         token_info = sp_oauth.get_access_token(code)
-        access_token = cached_token['access_token']
+        access_token = token_info['access_token']
 
     if access_token:
-        print "access token exists"
-        sp = spotipy.Spotify(access_token)
-        user = sp.current_user()
-        return render_template("profile.html", auth_url=auth_url,user=user)
+        # print "access token exists"
+        # sp = spotipy.Spotify(access_token)
+        # user = sp.current_user()
+        # user_id = user['id']
+        add_user_to_session()
+        limit = 8
+        results = sp.featured_playlists(locale=None, country=None, timestamp=None, limit=limit, offset=0)
+        if results:
+            playlists = []
+            i = 0
+
+            for i in range(limit):
+                playlists.append(results['playlists']['items'][i]['uri'])
+
+        return render_template("show-featured-playlists.html", auth_url=auth_url, playlists=playlists)
+
+@app.route('/logout')
+def logout():
+    session.clear()
+
+    return render_template("homepage.html",auth_url=auth_url)
 
 
 @app.route('/weather-playlist-lookup', methods=['GET'])
@@ -148,7 +155,6 @@ def lookup_playlists(weather_condition):
 
 
 
-
 @app.route('/sunny-playlists', methods=['GET'])
 def display_sunny_playlists():
     """Display sunny playlists without weather lookup"""
@@ -172,6 +178,7 @@ def display_rainy_playlists():
 
 ##################################################################################################
 """Helper functions"""
+
 def lookup_zipcode_key():
 
     zipcode = request.args.get("zipcode")
@@ -209,8 +216,19 @@ def authenticate_spotify():
     spotify = spotipy.Spotify(auth=token)
     return spotify
 
+def refresh():
+    # global token_info, sp
 
+    # if sp_oauth.is_token_expired(token_info):
+        token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
+        token = token_info['access_token']
+        sp = spotipy.Spotify(auth=token)
 
+def add_user_to_session():
+    sp = spotipy.Spotify(access_token)
+    user = sp.current_user()
+    user_id = user['id']
+    session['current_user'] = user_id
 
 
 
